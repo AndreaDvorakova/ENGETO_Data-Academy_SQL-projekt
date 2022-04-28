b0964596cbc73415605ea2fd991e8642a86a292b
AndreaDvorakova <100940898+AndreaDvorakova@users.noreply.github.com>
2022-04-28 17:45:40 +0200

    Add files via upload
    project_SQL secondary file - dodatecna data o dalsich evropskych statech

---
+++ ENGETO_t_andrea_dvorakova_project_SQL_secondary_final.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE TABLE t_andrea_dvorakova_project_SQL_secondary_final
+	SELECT e.country, 
+		c.continent , 
+		e.year, 
+		e.GDP,
+		e.gini, 
+		e.population 
+	FROM economies e 
+	JOIN countries c 
+		ON e.country = c.country 
+	WHERE c.continent = 'Europe' AND e.year >= 2006 
+		AND e.year <=2018
+	ORDER BY e.country, e.year;
+
