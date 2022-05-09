08050ed58d66df49e06762a84483eb3454f4452b
AndreaDvorakova <100940898+AndreaDvorakova@users.noreply.github.com>
2022-05-09 15:11:31 +0200

    Update ENGETO_t_andrea_dvorakova_project_SQL_primary_final.sql

--- ENGETO_t_andrea_dvorakova_project_SQL_primary_final.sql
+++ ENGETO_t_andrea_dvorakova_project_SQL_primary_final.sql
@@ -1,17 +1,17 @@
 CREATE OR REPLACE TABLE t_andrea_dvorakova_projekt_SQL_primary_final AS
-	SELECT cpric.year_czechia_price ,
-			czpay.industry_branch_code ,
+	SELECT cpric.year_czechia_price,
+			czpay.industry_branch_code,
 			czpay.name AS branch_name,
-			czpay.prum_mzda ,
-			cpric.category_code ,
+			czpay.prum_mzda,
+			cpric.category_code,
 			cpric.name AS category_name, 
-			cpric.prum_cena , 
-			cpric.price_value ,
-			cpric.price_unit ,
+			cpric.prum_cena, 
+			cpric.price_value,
+			cpric.price_unit,
 			econ.gdp 
 	FROM (
 		SELECT *,
-			ROUND(AVG(value),2) AS prum_mzda
+			ROUND(AVG(value), 2) AS prum_mzda
 		FROM czechia_payroll cp 
 		JOIN czechia_payroll_industry_branch cpib 
 			ON cp.industry_branch_code = cpib.code 
@@ -20,9 +20,9 @@ CREATE OR REPLACE TABLE t_andrea_dvorakova_projekt_SQL_primary_final AS
 		JOIN (
 		SELECT cp.category_code,
 			cpcat.name,
-			cpcat.price_value ,
-			cpcat.price_unit ,
-			round(AVG(value),2) AS prum_cena,
+			cpcat.price_value,
+			cpcat.price_unit,
+			round(AVG(value), 2) AS prum_cena,
 			YEAR(date_from) AS year_czechia_price
 		FROM czechia_price cp 
 		JOIN czechia_price_category cpcat 
@@ -36,4 +36,4 @@ CREATE OR REPLACE TABLE t_andrea_dvorakova_projekt_SQL_primary_final AS
 			ON econ.year = czpay.payroll_year 
 	ORDER BY cpric.year_czechia_price DESC,
 			czpay.industry_branch_code ASC,
-			cpric.category_code ; 
+			cpric.category_code; 
