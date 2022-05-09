48993274626c1dc222c62c7da34dd8b240dc1e19
AndreaDvorakova <100940898+AndreaDvorakova@users.noreply.github.com>
2022-05-09 15:12:02 +0200

    Update ENGETO_t_andrea_dvorakova_project_SQL_secondary_final.sql

--- ENGETO_t_andrea_dvorakova_project_SQL_secondary_final.sql
+++ ENGETO_t_andrea_dvorakova_project_SQL_secondary_final.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE TABLE t_andrea_dvorakova_project_SQL_secondary_final
 	SELECT e.country, 
-		c.continent , 
+		c.continent, 
 		e.year, 
 		e.GDP,
 		e.gini, 
@@ -9,6 +9,6 @@ CREATE OR REPLACE TABLE t_andrea_dvorakova_project_SQL_secondary_final
 	JOIN countries c 
 		ON e.country = c.country 
 	WHERE c.continent = 'Europe' AND e.year >= 2006 
-		AND e.year <=2018
+		AND e.year <= 2018
 	ORDER BY e.country, e.year;
 
